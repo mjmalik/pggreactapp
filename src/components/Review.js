4d39a7b7a93ebbcc4657b79621ca7e71a26c1d13
Musharraf Jamil <mjmalik777@gmail.com>
2019-01-03 10:59:08 +0500

    Links updated

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 const ReviewCard  = props => {
     if(props.data.status_code !== 200){
         return props.data.message;
     }
     const reviews = props.data.body.Results.map((review, index) => {
-        return <li key={index}>{review.title}</li>;
+        return <li key={index}>
+            <Link to={review.href}>{review.title}</Link>        
+        </li>;
     });
     return reviews;
 }
